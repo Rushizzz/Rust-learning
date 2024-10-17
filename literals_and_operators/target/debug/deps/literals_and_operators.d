272fd084784cc2e76101fccf18fa8852f01aafdf
Rushizzz <rushikeshshinde760@gmail.com>
2024-10-17 23:37:00 +0530

    learning literals

---
+++ literals_and_operators/target/debug/deps/literals_and_operators.d
@@ -0,0 +1,5 @@
+D:\Projects\Rust projects\literals_and_operators\target\debug\deps\literals_and_operators.exe: src/main.rs
+
+D:\Projects\Rust projects\literals_and_operators\target\debug\deps\literals_and_operators.d: src/main.rs
+
+src/main.rs:
---
+++ literals_and_operators/target/debug/literals_and_operators.d
@@ -0,0 +1,1 @@
+D:\Projects\Rust\ projects\literals_and_operators\target\debug\literals_and_operators.exe: D:\Projects\Rust\ projects\literals_and_operators\src\main.rs
